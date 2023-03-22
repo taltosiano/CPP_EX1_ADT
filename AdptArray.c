b41d21a913365d186a2a6f281c14d58b3233f3f8
taltosiano <94299489+taltosiano@users.noreply.github.com>
2023-03-22 16:18:02 +0200

    Update AdptArray.c

--- AdptArray.c
+++ AdptArray.c
@@ -31,46 +31,43 @@ typedef struct AdptArray_{    // building a structor of AdptArray
 void DeleteAdptArray(PAdptArray arrP){
     if(arrP == NULL || arrP->pElement == NULL)    // if the array or the elements are null we are not continue.
         return;
+
     for(int i = 0; i < arrP->sizeArr; i++){
         if(arrP->pElement[i] != NULL){        // we move element one by one and if he exist we delete the element.
             arrP->deleteFun(arrP->pElement[i]);
+            arrP->pElement[i] = NULL;
         }
     }
-    free(arrP->pElement);    // we release the array's element
-    free(arrP);           // then release the array is self.
+    free(arrP->pElement);    // free the array's data
+    free(arrP);           // then free the array.
 }
 
 // The function receives an index and an element and saves a copy of the element at this location.  
 // The function frees the old member if one exists
-Result SetAdptArray(PAdptArray arrP, int index, PElement element){
-    if(arrP == NULL || index < 0) {  // First we will check if something is wrong
+Result SetAdptArrayAt(PAdptArray arrP, int index, PElement element){
+   if(arrP == NULL || index < 0) {  // First we will check if something is wrong
         return FAIL;
     }
-    if(index >= arrP->sizeArr) {
-        PElement newArrP =(PElement*)calloc((index + 1), sizeof(PElement)); // create new extended array
+
+    if(index >= arrP->sizeArr) {     // if the index bigger then the array size we should extend the array
+        PElement* newArrP =(PElement*)calloc((index + 1), sizeof(PElement)); // create new extended array
         if(newArrP == NULL)
             return FAIL;
+
         memcpy(newArrP, arrP->pElement, (arrP->sizeArr) * sizeof(PElement));  // a copy function to new array
-        free(arrP->pElement);
+        free(arrP->pElement);     // realse the previous array
         arrP->pElement = newArrP;
-        arrP->sizeArr = index+1;
-        // for(int i = 0; i < arrP->sizeArr; i++) {
-        //     newArrP[i] = arrP->pElement[i];
-        // }
+        arrP->sizeArr = index+1;       // update the array size to be bigger
     }
-    arrP->pElement[index] = arrP->copyFun(element);
-    if(arrP->pElement[index] != NULL)
-        arrP->deleteFun(arrP->pElement[index]);
-    
-        // free(arrP->pElement);
-        // free(arrP);
-    arrP->pElement[index]=element;
+
+    if(arrP->pElement[index] != NULL)    
+        arrP->deleteFun(arrP->pElement[index]);   // delete the old element
+   
+    arrP->pElement[index] = arrP->copyFun(element);   // insert in the index location a copy of the new element
     return SUCCESS;
     
-    // arrP->pElement[index] = element;
-    // return SUCCESS;
  }
-
+ 
 // The function accepts an index and returns a copy of the member at that location if it exists
 PElement GetAdptArrayAt(PAdptArray arrP, int index){
  if(arrP == NULL || index >= arrP->sizeArr || index < 0 ) // First we will check if something is wrong
@@ -79,27 +76,28 @@ PElement GetAdptArrayAt(PAdptArray arrP, int index){
  if(arrP->pElement[index] == NULL)   // if there is not element in this index we return nothing-null.
     return NULL;
 
- return arrP->copyFun(arrP->pElement[index]);  // we use the exist copy function to return a COPY of this elemnt. 
+ return arrP->copyFun(arrP->pElement[index]);  // we use the copy function to return a COPY of this elemnt. 
  }
 
 // The function returns the size of the array (or -1 if it was not initialized successfully)
 int GetAdptArraySize(PAdptArray arrP){
-    if(arrP == NULL || arrP->pElement == NULL) 
+    if(arrP == NULL || arrP->pElement == NULL)  // check if the pointer is null or the array elements
       return -1;
     
-    return arrP->sizeArr;
+    return arrP->sizeArr;     // just return the array size
 
 }
 
 // The function prints the elements of the array
 void PrintDB(PAdptArray arrP){
-    if(arrP == NULL)
+    if(arrP == NULL)    // if the pointer is null the func stop
         return;
     for (int i = 0; i < arrP->sizeArr; i++)
     {
-        if(arrP->pElement[i] != NULL){          // if the element in this place is not null we'll print it
+        if(arrP->pElement[i] != NULL){          // only if the element in this place is not null we'll print it
             arrP->printFun(arrP->pElement[i]);
       }
     
     }
 }
+
